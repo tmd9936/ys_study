9e1f2b8d9aa64873feb21d200aaaa3ec7f43136d
tmd9936 <ghks9936@naver.com>
2020-11-06 17:11:45 +0900

    r프로그래밍 덩치

---
+++ algorithm02/덩치.py
@@ -0,0 +1,24 @@
+from sys import stdin
+
+n = int(stdin.readline())
+
+mans =[]
+
+for _ in range(n):
+    m = stdin.readline().split(' ')
+    mans.append([int(m[0]), int(m[1]), 0])
+
+for i in range(n):
+    for j in range(n):
+        if mans[i][0] > mans[j][0] and mans[i][1] > mans[j][1]:
+            mans[i][2] += 1
+
+for i in range(n):
+    rank = 1
+    for j in range(n):
+        if mans[i][2] < mans[j][2]:
+            rank += 1
+    print(rank, end=" ")
+
+
+    
--- algorithm02/하노이의탑.py
+++ algorithm02/하노이의탑.py
@@ -2,13 +2,25 @@ from sys import stdin
 
 k = int(stdin.readline())
 
-tower = [[x for x in range(k, 0, -1)] for _ in range(3)]
 
-for i in range(1,3):
-    for j in range(k):
-        tower[i][j] = 0
+# start에서 via를 거쳐 to로 보내기
+def hanoi(n, start, to, via):
+    if n == 1:
+        print(start, to)
+        #log.append([start, to])
+    else:
+        hanoi(n-1, start, via, to)
+        #log.append([start, to])
+        print(start, to)
+        hanoi(n-1, via, to, start)
 
+print(2**k-1)
+if k <=20:
+    hanoi(k,'1','3','2')
 
-print(tower)
-
-# def hanoi():
+# for i in range(0, len(log)):
+#     if i != len(log) -1:
+#         print(log[i][0], log[i][1])
+#     else:
+#         print(log[i][0], log[i][1], end="")
+        
